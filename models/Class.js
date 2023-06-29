e07b32a64338b7b4146bda4133b0cd01d5f6d34a
LeAnne B <branchleannelmb@gmail.com>
2023-06-29 14:22:12 -0400

    adds in chatgpt support

--- models/Class.js
+++ models/Class.js
@@ -4,24 +4,24 @@ const sequelize = require('../config/connection');
 class Class extends Model {}
 
 Class.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        class_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'class'
-    }
+    class_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'class',
+  },
 );
 
 module.exports = Class;
