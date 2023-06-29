3383a187b34e8304d68b347134be1fa609d3a5b5
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-29 09:53:00 -0500

    adds Class.js to models folder

---
+++ models/Class.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Class extends Model {}
+
+Class.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        class_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'class'
+    }
+);
+
+module.exports = Class;
