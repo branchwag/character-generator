041f24bd2e0616471f93f81f68e81b0991ee1ca8
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-29 18:58:16 -0500

    adds GET route for all races

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const chatgptRoutes = require('./chatgpt-routes');
 const classRoutes = require('./class-routes');
+const raceRoutes = require('./race-routes');
 
 router.use('/users', userRoutes);
 router.use('/chatgpt', chatgptRoutes);
 router.use('/classes', classRoutes);
+router.use('/races', raceRoutes)
 
 module.exports = router;
