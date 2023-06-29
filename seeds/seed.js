debc5f9caacb56ce9a7b235abe63832754a26d57
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-29 12:11:57 -0500

    connects classData to the seed.js file so that class can be seeded

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,13 @@
 const sequelize = require('../config/connection');
 const seedRace = require('./raceData');
-// const seedClass = require('./classData');
+const seedClass = require('./classData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedRace();
 
-  // await seedPaintings();
+  await seedClass();
 
   process.exit(0);
 };
