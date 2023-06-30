28927042fe1d0c6688c8664491465d39468141e2
Sameer Mirza <srmirza14@gmail.com>
2023-06-30 10:48:35 -0400

    integrated ai image gen | endpoint is working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,11 +5,13 @@ const chatgptRoutes = require('./chatgpt-routes');
 const classRoutes = require('./class-routes');
 const raceRoutes = require('./race-routes');
 const charRoutes = require('./character-routes');
+const openAiRoute = require('./openapi-image-routes');
 
 router.use('/users', userRoutes);
 router.use('/chatgpt', chatgptRoutes);
 router.use('/classes', classRoutes);
-router.use('/races', raceRoutes)
-router.use('/characters', charRoutes)
+router.use('/races', raceRoutes);
+router.use('/characters', charRoutes);
+router.use('/images', openAiRoute);
 
 module.exports = router;
