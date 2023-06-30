a89ab88a7e999464df9b4cd4f3b49b20194ccc13
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-29 19:44:26 -0500

    connects users files so that POST request works in insomnia to add a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,17 +4,24 @@ const { User } = require('../../models');
 // CREATE new user
 router.post('/', async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    // const dbUserData = await User.create({
+    //   username: req.body.username,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    // });
 
+    // req.session.save(() => {
+    //   req.session.loggedIn = true;
+
+    //   res.status(200).json(dbUserData);
+    // });
+    const userData = await User.create(req.body);
     req.session.save(() => {
-      req.session.loggedIn = true;
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
-      res.status(200).json(dbUserData);
-    });
+      res.status(200).json(userData)
+    })
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
