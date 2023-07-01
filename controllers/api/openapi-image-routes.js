d85944daf6c1380c85fc3b0f8f86b08997244149
Sameer Mirza <srmirza14@gmail.com>
2023-07-01 13:43:06 -0400

    integrated new api | rapid api - ARImageSynthesizer

--- controllers/api/openapi-image-routes.js
+++ controllers/api/openapi-image-routes.js
@@ -1,36 +1,61 @@
 const router = require('express').Router();
+const imagapiKey = process.env.IMAGE_API_KEY;
+let reqHash = '';
 
 // /api/images/imagegen
 router.post('/imagegen', async (req, res) => {
-  const url =
-    'https://image-generator-using-text-descriptions-fast-cheap3.p.rapidapi.com/v1/inference/runwayml/stable-diffusion-v1-5';
+  const url = 'https://arimagesynthesizer.p.rapidapi.com/generate';
   const options = {
     method: 'POST',
     headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'ea7d8918bcmsh2f4999342dafc11p1b9681jsn5835d1387bcd',
-      'X-RapidAPI-Host':
-        'image-generator-using-text-descriptions-fast-cheap3.p.rapidapi.com',
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': `${imagapiKey}`,
+      'X-RapidAPI-Host': 'arimagesynthesizer.p.rapidapi.com',
     },
-    body: JSON.stringify({
-      prompt: 'cyberpunk city 8k',
-      negative_prompt:
-        'worst quality, low quality, blurry, deformed iris, deformed pupils, bad eyes, cross eyed, poorly drawn face, cloned face, extra fingers, mutated hands, fused fingers, too many fingers, missing arms, missing legs, extra arms, extra legs, poorly drawn hands, bad anatomy, bad proportions, cropped, lowres, text, jpeg artifacts, signature, watermark, username, artist name, trademark, watermark, title, multiple view, Reference sheet, long neck, Out of Frame, Nude, NSFW, Erotica',
-      width: 512,
-      height: 512,
-      num_inference_steps: 50,
-      guidance_scale: 7,
-      width: 512,
-      height: 512,
-      seed: 0,
+    body: new URLSearchParams({
+      prompt: '',
+      id: '12345',
+      width: '768',
+      height: '768',
+      inferenceSteps: '50',
+      guidanceScale: '7.5',
+      img2img_strength: '0.75',
     }),
   };
 
   try {
     const response = await fetch(url, options);
-    const result = await response.json();
+    const result = await response.text();
+    const imgHash = JSON.parse(result);
     res.send(result);
-    console.log(result.images);
+    console.log(result);
+    console.log(imgHash.hash);
+    reqHash = imgHash.hash;
+    console.log(reqHash);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// /api/images/genimg
+
+router.get('/genimg', async (req, res) => {
+  const url = `https://arimagesynthesizer.p.rapidapi.com/get?hash=${reqHash}&returnType=image`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ea7d8918bcmsh2f4999342dafc11p1b9681jsn5835d1387bcd',
+      'X-RapidAPI-Host': 'arimagesynthesizer.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response;
+    const encodeUrl = btoa(url);
+    res.send(result);
+    console.log(result);
+    console.log(encodeUrl);
   } catch (error) {
     console.error(error);
   }
