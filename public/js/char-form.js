e50ae75a09e219f1e584e4013bfabc448ef53d21
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-02 17:45:24 -0500

    adds an event listener to char-form.js file

--- public/js/char-form.js
+++ public/js/char-form.js
@@ -2,7 +2,13 @@ const charFormHandler = async (event) => {
     console.log('called charFormHandler');
 
     event.preventDefault();
+    console.log('submit button clicked')
 
     // Collect values from the character form
-    const 
+    const character_gender = document.querySelector('#gender').value
+    console.log(character_gender)
 }
+
+document
+    .querySelector('#submitBtn')
+    .addEventListener('submit', charFormHandler)
