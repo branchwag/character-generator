7b9839f88254df114e83ef28e95c272274d3e115
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-03 16:15:33 -0500

    submit button adds character data to database and moves user to the output page

--- public/js/char-form.js
+++ public/js/char-form.js
@@ -16,6 +16,9 @@
 const submitButton = document.querySelector('#submitBtn')
 const gender = document.querySelector('#gender')
 const eye = document.querySelector('#eye-color')
+const hair = document.querySelector('#hair-color')
+const race = document.querySelector('#race')
+const charClass = document.querySelector('#class')
 
 
 // const class_id = document.querySelector('#class').value
@@ -33,20 +36,26 @@ const eye = document.querySelector('#eye-color')
 
 const collectCharData = async () => {
   const character_gender = gender.value
-  // const eye_color = eye.find("option:selected").text()
+  const eye_color = eye.options[eye.selectedIndex].text
+  const hair_color = hair.options[hair.selectedIndex].text
+  const race_id = race.selectedIndex
+  const class_id = charClass.selectedIndex
   console.log(character_gender)
-  console.log(eye.options[eye.selectedIndex].text)
-  // const addChar = await fetch('/api/characters', {
-  //   method: 'POST',
-  //   body: JSON.stringify({ character_gender }),
-  //   headers: { 'Content-Type': 'application/json' }
-  // });
-  // if (addChar.ok) {
-  //   document.location.replace('/partials/output')
-  //   console.log("all good")
-  // } else {
-  //   console.log("unsuccessful post request")
-  // }
+  console.log(eye_color)
+  console.log(hair_color)
+  console.log(race_id)
+  console.log(class_id)
+  const addChar = await fetch('/api/characters', {
+    method: 'POST',
+    body: JSON.stringify({ character_gender, eye_color, hair_color, race_id, class_id }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+  if (addChar.ok) {
+    document.location.replace('/output')
+    console.log("all good")
+  } else {
+    console.log("unsuccessful post request")
+  }
 }
 
 function charFormHandler(event) {
