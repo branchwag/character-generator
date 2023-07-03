1111f535f66faadf3c381aa51c7e14e53b6319ce
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-03 14:21:35 -0500

    connects submit button to char-form.js

--- public/js/char-form.js
+++ public/js/char-form.js
@@ -1,14 +1,39 @@
-const charFormHandler = async (event) => {
-  console.log('called charFormHandler');
+// const charFormHandler = async (event) => {
+//   console.log('called charFormHandler');
 
+//   event.preventDefault();
+//   console.log('submit button clicked');
+
+//   // Collect values from the character form
+//   const character_gender = document.querySelector('#gender').value;
+//   console.log(character_gender);
+// };
+
+// document
+//   .querySelector('#submitBtn')
+//   .addEventListener('submit', charFormHandler);
+
+const submitButton = document.querySelector('#submitBtn')
+const character_gender = document.querySelector('#gender')
+
+
+// function loadOutputPage() { 
+  
+//   console.log("Switch to output")
+//   window.location.href = "partials/output"
+// }
+
+function collectCharData() {
+  console.log(character_gender.value)
+}
+
+function charFormHandler(event) {
   event.preventDefault();
-  console.log('submit button clicked');
+  console.log('clicked')
+  collectCharData()
+  // loadOutputPage()
+}
 
-  // Collect values from the character form
-  const character_gender = document.querySelector('#gender').value;
-  console.log(character_gender);
-};
+submitButton.addEventListener('click', charFormHandler)
 
-document
-  .querySelector('#submitBtn')
-  .addEventListener('submit', charFormHandler);
+console.log("green eggs and ham")
