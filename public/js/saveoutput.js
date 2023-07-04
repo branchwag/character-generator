153dde0b72ffae58442e100970ef0a6dfa00f7a5
LeAnne B <branchleannelmb@gmail.com>
2023-07-04 12:39:49 -0400

    able to scrap data from page to send in POST request

--- public/js/saveoutput.js
+++ public/js/saveoutput.js
@@ -1,15 +1,20 @@
-const submitButton = document.querySelector('#save');
-const charNameEl = document.querySelector('#genname');
-const charBackstoryEl = document.querySelector('#backstoryoutput');
 // const charImageEl = document.querySelector('#genimage'); Need to get image generated on page
+// const charName = charNameEl.textContent;
+// const charBackstory = charBackstoryEl.textContent;
+const submitButton = document.querySelector('#save');
 
 const collectCharData = async () => {
+  const charNameEl = document.querySelector('#genname');
+  const charBackstoryEl = document.querySelector('#backstoryoutput');
   const charName = charNameEl.textContent;
   const charBackstory = charBackstoryEl.textContent;
+  console.log(charName);
+  //   console.log(charNameEl);
   //const charImg = get the source here;
 
   if (charName && charBackstory) {
     const addCharGen = await fetch('/api/characters/1', {
+      //id needs to be dynamically generated
       method: 'POST',
       body: JSON.stringify({
         charName,
