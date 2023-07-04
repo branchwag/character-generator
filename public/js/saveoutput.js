4c752fdd74124d9b2a2df7236a8662e57e1791e9
LeAnne B <branchleannelmb@gmail.com>
2023-07-04 11:45:12 -0400

    created start of saveoutput js script

---
+++ public/js/saveoutput.js
@@ -0,0 +1,45 @@
+const submitButton = document.querySelector('#save');
+const charName = document.querySelector('#genname');
+const charBackstory = document.querySelector('#eye-color');
+const charImage = document.querySelector('#hair-color');
+
+const collectCharData = async () => {
+  const character_gender = gender.value;
+  const eye_color = eye.options[eye.selectedIndex].text;
+  const hair_color = hair.options[hair.selectedIndex].text;
+  const race_id = race.selectedIndex;
+  const class_id = charClass.selectedIndex;
+
+  if (character_gender && class_id && race_id) {
+    const addChar = await fetch('/api/characters', {
+      method: 'POST',
+      body: JSON.stringify({
+        character_gender,
+        eye_color,
+        hair_color,
+        race_id,
+        class_id,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (addChar.ok) {
+      document.location.replace('/output');
+      console.log('all good');
+    } else {
+      console.log('unsuccessful post request');
+    }
+  } else {
+    alert('Gender, race, and class are required fields');
+  }
+};
+
+function charFormHandler(event) {
+  event.preventDefault();
+  console.log('clicked');
+  collectCharData();
+  // loadOutputPage()
+}
+
+submitButton.addEventListener('click', charFormHandler);
+
+console.log('green eggs and ham');
