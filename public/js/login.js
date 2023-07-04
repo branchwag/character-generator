340377292b44e7ffd4557f2b16c19b2c7729e071
Calvin Roberts <you@example.com>
2023-07-04 00:16:22 -0600

    changed login failure alert

--- public/js/login.js
+++ public/js/login.js
@@ -21,7 +21,7 @@ const loginFormHandler = async (event) => {
       // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
+      alert('Login failed! Incorrect username or password.');
     }
   }
 };
