b2529ed8ad36ad5d28fe44dcf008bc3911a5e231
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-05 18:07:59 -0500

    adds GET routes for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -81,4 +81,25 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.get('/', async (req, res) => {
+  // try {
+  const userAll = await User.findAll();
+  const users = userAll.map((User) => User.get({ plain: true }));
+  res.send(users);
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with that id' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
